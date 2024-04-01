0a2f03523d4de722d104030c78048e3356bb213f
Nitin Sharma <nitinshr135@gmail.com>
2024-04-02 02:08:46 +0530

    Init commit for upload blob

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,63 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@radix-ui/themes";
+import { useRef } from "react";
 
 export default function Home() {
+  const hiddenFileInput = useRef<HTMLInputElement>(null);
+
+  const handleUploadClick = () => {
+    if (hiddenFileInput.current) hiddenFileInput.current.click();
+  };
+
+  const FILE_SIZE_LIMIT = 5 * 1000 * 1024;
+
+  const handleChange = (event: any) => {
+    const fileUploaded = event.target.files[0];
+    if (event.target.files[0].size >= FILE_SIZE_LIMIT) {
+      alert("File with maximum size of 5MB is allowed");
+    }
+    console.log(fileUploaded);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="w-full items-center justify-between font-mono text-sm lg:flex">
+        <div>Upload File</div>
+        <Button
+          size="4"
+          variant="solid"
+          className="!cursor-pointer"
+          onClick={handleUploadClick}
+        >
+          Upload
+        </Button>
+        <input
+          type="file"
+          onChange={handleChange}
+          ref={hiddenFileInput}
+          className="hidden"
         />
       </div>
+      {/* 
+      <div
+        className="flex flex-col items-center text-center justify-center gap-4
+              py-4 rounded border border-form-selected border-dashed relative
+              bg-bg-fileUpload dark:bg-gray-800"
+      >
+        <input
+          type="file"
+          id="fileupload"
+          className="absolute left-0 top-0 opacity-0 cursor-pointer w-full h-full"
+          onChange={handleFileChange}
+          multiple
+          value=""
+          title=""
+        />
+        <img src={UploadFileIcon} alt="upload" />
+        <span className="font-semibold text-text-uploadLink">
+          Click to Select
+        </span>
+      </div> */}
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
