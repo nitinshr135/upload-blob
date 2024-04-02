922f1f057222e3792c4eba797e1ccb4cd26e3396
Nitin Sharma <nitinshr135@gmail.com>
2024-04-02 14:37:07 +0530

    Add service route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
+import { upload } from "@vercel/blob/client";
 import { useRef } from "react";
+import { UploadIcon } from "@radix-ui/react-icons";
+import TableComponent from "./components/table-component";
 
 export default function Home() {
   const hiddenFileInput = useRef<HTMLInputElement>(null);
@@ -11,12 +14,17 @@ export default function Home() {
 
   const FILE_SIZE_LIMIT = 5 * 1000 * 1024;
 
-  const handleChange = (event: any) => {
+  const handleChange = async (event: any) => {
     const fileUploaded = event.target.files[0];
     if (event.target.files[0].size >= FILE_SIZE_LIMIT) {
       alert("File with maximum size of 5MB is allowed");
+    } else {
+      const newBlob = await upload(fileUploaded.name, fileUploaded, {
+        access: "public",
+        handleUploadUrl: "/api/upload",
+      });
+      console.log(newBlob);
     }
-    console.log(fileUploaded);
   };
 
   return (
@@ -29,6 +37,7 @@ export default function Home() {
           className="!cursor-pointer"
           onClick={handleUploadClick}
         >
+          <UploadIcon />
           Upload
         </Button>
         <input
@@ -38,96 +47,7 @@ export default function Home() {
           className="hidden"
         />
       </div>
-      {/* 
-      <div
-        className="flex flex-col items-center text-center justify-center gap-4
-              py-4 rounded border border-form-selected border-dashed relative
-              bg-bg-fileUpload dark:bg-gray-800"
-      >
-        <input
-          type="file"
-          id="fileupload"
-          className="absolute left-0 top-0 opacity-0 cursor-pointer w-full h-full"
-          onChange={handleFileChange}
-          multiple
-          value=""
-          title=""
-        />
-        <img src={UploadFileIcon} alt="upload" />
-        <span className="font-semibold text-text-uploadLink">
-          Click to Select
-        </span>
-      </div> */}
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <TableComponent />
     </main>
   );
 }
